ea175a942efb285ad5ada0c303ab7ad934b37780
Dominykas Blyžė <hello@dominykas.com>
2012-11-19 18:39:39 +0200

    promise gets resolved asynchronously

--- 01-errorHandlingWithPromises.js
+++ 01-errorHandlingWithPromises.js
@@ -1,15 +1,31 @@
 var Q = require("q");
 var trolinkGapsi = function()
 {
-	return Q.resolve("Eina į prokuratūrą");
+	var g = Q.defer();
+
+	// this setTimeout() to break the event loop is actually meaningless,
+	// since promise handlers get called asynchronously anyways
+	setTimeout(function(){
+		g.resolve("Eina į prokuratūrą");
+	}, 100);
+
+	return g.promise;
 };
 
 trolinkGapsi()
+
+	// it does not matter when the promise gets resolved - .then() will get called as soon as it does
+	// onTrollCompleted() will also _always_ be called asynchronously, i.e. not in the same tick
 	.then(function onTrollCompleted(kaDaroGapsys){
 		if (kaDaroGapsys == "Eina į prokuratūrą")
 			throw new Error("Skandalas žiniasklaidoje");
 	})
+
+	// whenever the promise handler throws an exception - it will _always_ be passed along to the failure
+	// handler, if one exists
 	.fail(function onTrollFailed(e){
 		console.log("Viešieji ryšiai suvaldyti", e);
 	})
+
+	// IMPORTANT: do not forget to call .done() - otherwise unhandled errors will go undetected!
 	.done();
