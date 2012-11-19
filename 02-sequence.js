25edbf4dd6d0177294fb38f55a2f709066125365
Dominykas Blyžė <hello@dominykas.com>
2012-11-19 19:31:27 +0200

    sequence example done

--- 02-sequence.js
+++ 02-sequence.js
@@ -1,4 +1,34 @@
+var pazadai = require("./pazadai");
+
 module.exports = function(callback)
 {
+	pazadai.rinkimai(function(e, kiekMandatu){
 
+		if (e) {
+			callback(e);
+			return;
+		}
+
+		if (kiekMandatu < 5) {
+			callback(new Error("Negalim formuot koalicijos!"));
+			return;
+		}
+
+		pazadai.koalicija(kiekMandatu, function(e, arSuSocDemais){
+
+			if (e) {
+				callback(e);
+				return;
+			}
+
+			if (!arSuSocDemais) {
+				callback(new Error("Partneriai nesutinka"));
+				return;
+			}
+
+			callback(null, 1509);
+
+		});
+
+	});
 };
