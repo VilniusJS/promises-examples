75b1a7776cd3e12215fb5cd7274cf73a65a78a35
Dominykas Blyžė <hello@dominykas.com>
2012-11-19 19:42:52 +0200

    sequence with promise example impl

--- 03-sequenceWithPromises.js
+++ 03-sequenceWithPromises.js
@@ -4,5 +4,23 @@ var pazadai = require("./pazadai");
 module.exports = function()
 {
 
+	var promise = pazadai.rinkimai()
+
+		.then(function(kiekIsrinktu){
+
+			if (kiekIsrinktu < 5) throw new Error("Negalim formuot koalicijos!");
+			return kiekIsrinktu;
+
+		})
+
+		.then(pazadai.koalicija)
+
+		.then(function(arSuSocDemais){
+			if (!arSuSocDemais) throw new Error("Partneriai nesutinka");
+
+			return 1509;
+		});
+
+	return promise;
 
 };
