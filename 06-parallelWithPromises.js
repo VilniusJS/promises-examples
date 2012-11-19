f4b9dbdae2af9e2483b959937700a6d2e57b601b
Dominykas Blyžė <hello@dominykas.com>
2012-11-19 20:27:05 +0200

    Q.allResolved().spread() like a boss

--- 06-parallelWithPromises.js
+++ 06-parallelWithPromises.js
@@ -4,5 +4,14 @@ var Q = require("q");
 module.exports = function(iSeima, iKalejima)
 {
 
+	Q.allResolved([pazadai.pakelkMinimaliaAlga(), pazadai.ginirokIkanomika(), pazadai.dokDarbasVisiems()])
+		.spread(function(alga, augymas, nidarbas){
+			if (alga >= 1509 && augymas >= 10 && nidarbas == 0) {
+				iSeima();
+			} else {
+				iKalejima();
+			}
+		})
+		.fail(iKalejima).done();
 
 };
