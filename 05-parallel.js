c2a7f0c9830056805a41c80ab0dfa0349b0e9a88
Dominykas Blyžė <hello@dominykas.com>
2012-11-19 20:19:59 +0200

    parallel example done

--- 05-parallel.js
+++ 05-parallel.js
@@ -3,5 +3,35 @@ var pazadai = require("./pazadai");
 module.exports = function(iSeima, iKalejima)
 {
 
+	var alga, augymas, nidarbas, kaipSekasi=0;
+
+	var arJauRojusLietuvoje = function()
+	{
+		if (kaipSekasi < 3) return;
+
+		if (alga >= 1509 && augymas >= 10 && nidarbas == 0) {
+			iSeima();
+		} else {
+			iKalejima();
+		}
+	};
+
+	pazadai.pakelkMinimaliaAlga(function(v){
+		alga=v;
+		kaipSekasi++;
+		arJauRojusLietuvoje();
+	});
+
+	pazadai.ginirokIkanomika(function(v){
+		augymas=v;
+		kaipSekasi++;
+		arJauRojusLietuvoje();
+	});
+
+	pazadai.dokDarbasVisiems(function(v){
+		nidarbas=v;
+		kaipSekasi++;
+		arJauRojusLietuvoje();
+	});
 
 };
