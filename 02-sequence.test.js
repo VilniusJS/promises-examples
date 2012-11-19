ed009c6198f415b6a7662b46f0756906e16f5a8c
Dominykas Blyžė <hello@dominykas.com>
2012-11-19 19:17:18 +0200

    empty example 02

---
+++ 02-sequence.test.js
@@ -0,0 +1,12 @@
+var buster = require("buster");
+var pazadai = require("./pazadai");
+var pakelkAlga = require("./02-sequence");
+
+buster.testCase("Kaip pakelsim minimalią algą?", {
+
+	"Kai rinkimuose klaida - algos kelti negalim": function(done)
+	{
+
+	}
+
+});
